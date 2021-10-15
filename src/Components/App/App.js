6b1439c390bd46989ef0adfb614a734eaf11e4d1
quocdat202 <quocdat732@gmail.com>
2021-10-15 23:03:12 +0700

    Upload Header Lappe

---
+++ src/Components/App/App.js
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+// import logo from "./logo.svg";
+// import './App.css';
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="menu">
+        <div className="menu-qc">
+          <a className="tinqc" href="#">
+            Tin quảng cáo: bla bla...........
+          </a>
+          <ul>
+            <li>
+              <a href="#">
+                <img src="images/icons8_phone_48.png" alt="tuvanmuahang" />
+                Tư vấn mua hàng: 0988888288
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="images/icons8_phone_48.png" alt="cskh" />
+                CSKH: 0988888288
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="images/responsive_48px.png" alt="tincongnghe" />
+                Tin công nghệ
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="menu-top">
+          <ul className="menu-left">
+            <li>
+              <a href="#">
+                <img src="images/Logo (2).png" alt="" />
+              </a>
+            </li>
+            <li className="li-input">
+              <input type="text" placeholder="Search" />
+            </li>
+            {/* <button><img src="" alt=""></button> */}
+          </ul>
+          <ul className="menu-right">
+            <li>
+              <a href="#">
+                <img src="images/home_24px.png" alt="" />
+                Trang Chủ
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="images/menu.png" alt="" />
+                Sản Phẩm
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="images/notification_30px.png" alt="" />
+                Thông Báo
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="images/shopping_24px.png" alt="" />
+                Giỏ Hàng
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="images/user.png" alt="" />
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
